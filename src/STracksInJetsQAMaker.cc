56da30de536a86b463c282e9402dbda1bebfc012
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-03-25 10:05:48 -0400

    Create skeleton of module

--- src/STracksInJetsQAMaker.cc
+++ src/STracksInJetsQAMaker.cc
@@ -1,136 +1,98 @@
-//____________________________________________________________________________..
+// ----------------------------------------------------------------------------
+// 'STracksInJetsQAMaker.cc'
+// Derek Anderson
+// 03.25.2024
 //
-// This is a template for a Fun4All SubsysReco module with all methods from the
-// $OFFLINE_MAIN/include/fun4all/SubsysReco.h baseclass
-// You do not have to implement all of them, you can just remove unused methods
-// here and in STracksInJetsQAMaker.h.
-//
-// STracksInJetsQAMaker(const std::string &name = "STracksInJetsQAMaker")
-// everything is keyed to STracksInJetsQAMaker, duplicate names do work but it makes
-// e.g. finding culprits in logs difficult or getting a pointer to the module
-// from the command line
-//
-// STracksInJetsQAMaker::~STracksInJetsQAMaker()
-// this is called when the Fun4AllServer is deleted at the end of running. Be
-// mindful what you delete - you do loose ownership of object you put on the node tree
-//
-// int STracksInJetsQAMaker::Init(PHCompositeNode *topNode)
-// This method is called when the module is registered with the Fun4AllServer. You
-// can create historgrams here or put objects on the node tree but be aware that
-// modules which haven't been registered yet did not put antyhing on the node tree
-//
-// int STracksInJetsQAMaker::InitRun(PHCompositeNode *topNode)
-// This method is called when the first event is read (or generated). At
-// this point the run number is known (which is mainly interesting for raw data
-// processing). Also all objects are on the node tree in case your module's action
-// depends on what else is around. Last chance to put nodes under the DST Node
-// We mix events during readback if branches are added after the first event
-//
-// int STracksInJetsQAMaker::process_event(PHCompositeNode *topNode)
-// called for every event. Return codes trigger actions, you find them in
-// $OFFLINE_MAIN/include/fun4all/Fun4AllReturnCodes.h
-//   everything is good:
-//     return Fun4AllReturnCodes::EVENT_OK
-//   abort event reconstruction, clear everything and process next event:
-//     return Fun4AllReturnCodes::ABORT_EVENT; 
-//   proceed but do not save this event in output (needs output manager setting):
-//     return Fun4AllReturnCodes::DISCARD_EVENT; 
-//   abort processing:
-//     return Fun4AllReturnCodes::ABORT_RUN
-// all other integers will lead to an error and abort of processing
-//
-// int STracksInJetsQAMaker::ResetEvent(PHCompositeNode *topNode)
-// If you have internal data structures (arrays, stl containers) which needs clearing
-// after each event, this is the place to do that. The nodes under the DST node are cleared
-// by the framework
-//
-// int STracksInJetsQAMaker::EndRun(const int runnumber)
-// This method is called at the end of a run when an event from a new run is
-// encountered. Useful when analyzing multiple runs (raw data). Also called at
-// the end of processing (before the End() method)
-//
-// int STracksInJetsQAMaker::End(PHCompositeNode *topNode)
-// This is called at the end of processing. It needs to be called by the macro
-// by Fun4AllServer::End(), so do not forget this in your macro
-//
-// int STracksInJetsQAMaker::Reset(PHCompositeNode *topNode)
-// not really used - it is called before the dtor is called
-//
-// void STracksInJetsQAMaker::Print(const std::string &what) const
-// Called from the command line - useful to print information when you need it
-//
-//____________________________________________________________________________..
+// A small Fun4All module to produce QA plots for tracks,
+// hits, and more.
+// ----------------------------------------------------------------------------
 
+#define STRACKSINJETSQAMAKER_CC
+
+// module defintion
 #include "STracksInJetsQAMaker.h"
 
-#include <fun4all/Fun4AllReturnCodes.h>
 
-#include <phool/PHCompositeNode.h>
 
-//____________________________________________________________________________..
-STracksInJetsQAMaker::STracksInJetsQAMaker(const std::string &name):
- SubsysReco(name)
-{
+// ctor/dtor ------------------------------------------------------------------
+
+STracksInJetsQAMaker::STracksInJetsQAMaker(const std::string& name, const std::string& sOutFileName) : SubsysReco(name) {
+
   std::cout << "STracksInJetsQAMaker::STracksInJetsQAMaker(const std::string &name) Calling ctor" << std::endl;
-}
 
-//____________________________________________________________________________..
-STracksInJetsQAMaker::~STracksInJetsQAMaker()
-{
+}  // end ctor(std::string, st::string)
+
+
+
+STracksInJetsQAMaker::~STracksInJetsQAMaker() {
+
   std::cout << "STracksInJetsQAMaker::~STracksInJetsQAMaker() Calling dtor" << std::endl;
-}
 
-//____________________________________________________________________________..
-int STracksInJetsQAMaker::Init(PHCompositeNode *topNode)
-{
-  std::cout << "STracksInJetsQAMaker::Init(PHCompositeNode *topNode) Initializing" << std::endl;
+}  // end dtor
+
+
+
+// fun4all methods ------------------------------------------------------------
+
+int STracksInJetsQAMaker::Init(PHCompositeNode* topNode) {
+
+  std::cout << "STracksInJetsQAMaker::Init(PHCompositeNode* topNode) Initializing" << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
-}
 
-//____________________________________________________________________________..
-int STracksInJetsQAMaker::InitRun(PHCompositeNode *topNode)
-{
-  std::cout << "STracksInJetsQAMaker::InitRun(PHCompositeNode *topNode) Initializing for Run XXX" << std::endl;
+}  // end 'Init(PHCompositeNode*)'
+
+
+
+int STracksInJetsQAMaker::InitRun(PHCompositeNode* topNode) {
+
+  std::cout << "STracksInJetsQAMaker::InitRun(PHCompositeNode* topNode) Initializing for Run XXX" << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
-}
 
-//____________________________________________________________________________..
-int STracksInJetsQAMaker::process_event(PHCompositeNode *topNode)
-{
-  std::cout << "STracksInJetsQAMaker::process_event(PHCompositeNode *topNode) Processing Event" << std::endl;
+}  // end 'InitRun(PHCompositeNode*)'
+
+
+
+int STracksInJetsQAMaker::process_event(PHCompositeNode* topNode) {
+
+  std::cout << "STracksInJetsQAMaker::process_event(PHCompositeNode* topNode) Processing Event" << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
-}
 
-//____________________________________________________________________________..
-int STracksInJetsQAMaker::ResetEvent(PHCompositeNode *topNode)
-{
-  std::cout << "STracksInJetsQAMaker::ResetEvent(PHCompositeNode *topNode) Resetting internal structures, prepare for next event" << std::endl;
+}  // end 'process_event(PHCompositeNode*)'
+
+
+
+int STracksInJetsQAMaker::ResetEvent(PHCompositeNode* topNode) {
+
+  std::cout << "STracksInJetsQAMaker::ResetEvent(PHCompositeNode* topNode) Resetting internal structures, prepare for next event" << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
-}
 
-//____________________________________________________________________________..
-int STracksInJetsQAMaker::EndRun(const int runnumber)
-{
+}  // end 'ResetEvent(PHCompositeNode*)'
+
+
+
+int STracksInJetsQAMaker::EndRun(const int runnumber) {
+
   std::cout << "STracksInJetsQAMaker::EndRun(const int runnumber) Ending Run for Run " << runnumber << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
-}
 
-//____________________________________________________________________________..
-int STracksInJetsQAMaker::End(PHCompositeNode *topNode)
-{
-  std::cout << "STracksInJetsQAMaker::End(PHCompositeNode *topNode) This is the End..." << std::endl;
+}  // end 'EndRun(int)'
+
+
+
+int STracksInJetsQAMaker::End(PHCompositeNode* topNode) {
+
+  std::cout << "STracksInJetsQAMaker::End(PHCompositeNode* topNode) This is the End..." << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
-}
 
-//____________________________________________________________________________..
-int STracksInJetsQAMaker::Reset(PHCompositeNode *topNode)
-{
- std::cout << "STracksInJetsQAMaker::Reset(PHCompositeNode *topNode) being Reset" << std::endl;
+}  // end 'End(PHCompositeNode*)'
+
+
+
+int STracksInJetsQAMaker::Reset(PHCompositeNode* topNode) {
+
+  std::cout << "STracksInJetsQAMaker::Reset(PHCompositeNode* topNode) being Reset" << std::endl;
   return Fun4AllReturnCodes::EVENT_OK;
-}
 
-//____________________________________________________________________________..
-void STracksInJetsQAMaker::Print(const std::string &what) const
-{
-  std::cout << "STracksInJetsQAMaker::Print(const std::string &what) const Printing info for " << what << std::endl;
-}
+}  // end 'Reset(PHCompositeNode*)'
+
+// end ------------------------------------------------------------------------
--- src/STracksInJetsQAMaker.h
+++ src/STracksInJetsQAMaker.h
@@ -1,56 +1,62 @@
 // Tell emacs that this is a C++ source
 //  -*- C++ -*-.
+// ----------------------------------------------------------------------------
+// 'STracksInJetsQAMaker.h'
+// Derek Anderson
+// 03.25.2024
+//
+// A small Fun4All module to produce QA plots for tracks,
+// hits, and more.
+// ----------------------------------------------------------------------------
+
 #ifndef STRACKSINJETSQAMAKER_H
 #define STRACKSINJETSQAMAKER_H
 
-#include <fun4all/SubsysReco.h>
-
+// c++ utilities
 #include <string>
+#include <vector>
+#include <utility>
+// root libraries
+#include <TH1.h>
+#include <TH2.h>
+#include <TFile.h>
+// fun4all libraries
+#include <fun4all/SubsysReco.h>
+#include <phool/PHCompositeNode.h>
+#include <fun4all/Fun4AllReturnCodes.h>
 
-class PHCompositeNode;
 
-class STracksInJetsQAMaker : public SubsysReco
-{
- public:
 
-  STracksInJetsQAMaker(const std::string &name = "STracksInJetsQAMaker");
+// STracksInJetsQAMaker definition --------------------------------------------
 
-  ~STracksInJetsQAMaker() override;
+class STracksInJetsQAMaker : public SubsysReco {
 
-  /** Called during initialization.
-      Typically this is where you can book histograms, and e.g.
-      register them to Fun4AllServer (so they can be output to file
-      using Fun4AllServer::dumpHistos() method).
-   */
-  int Init(PHCompositeNode *topNode) override;
+  public:
 
-  /** Called for first event when run number is known.
-      Typically this is where you may want to fetch data from
-      database, because you know the run number. A place
-      to book histograms which have to know the run number.
-   */
-  int InitRun(PHCompositeNode *topNode) override;
+    // ctor/dtor
+    STracksInJetsQAMaker(const std::string &name = "STracksInJetsQAMaker", const std::string& sOutFileName = "tracksinjets.root");
+    ~STracksInJetsQAMaker() override;
 
-  /** Called for each event.
-      This is where you do the real work.
-   */
-  int process_event(PHCompositeNode *topNode) override;
+    // f4a methods
+    int  Init(PHCompositeNode* topNode)          override;
+    int  InitRun(PHCompositeNode* topNode)       override;
+    int  process_event(PHCompositeNode* topNode) override;
+    int  ResetEvent(PHCompositeNode* topNode)    override;
+    int  EndRun(const int runnumber)             override;
+    int  End(PHCompositeNode* topNode)           override;
+    int  Reset(PHCompositeNode*  /*topNode*/)    override;
 
-  /// Clean up internals after each event.
-  int ResetEvent(PHCompositeNode *topNode) override;
+   private:
 
-  /// Called at the end of each run.
-  int EndRun(const int runnumber) override;
+     // io members
+     TFile* m_outFile = NULL;
 
-  /// Called at the end of all processing.
-  int End(PHCompositeNode *topNode) override;
+     // top level information
+     int64_t m_runNum = 0;
+     int64_t m_evtNum = 0;
 
-  /// Reset
-  int Reset(PHCompositeNode * /*topNode*/) override;
+};  // end STracksInJetsQAMaker
 
-  void Print(const std::string &what = "ALL") const override;
+#endif
 
- private:
-};
-
-#endif // STRACKSINJETSQAMAKER_H
+// end ------------------------------------------------------------------------
