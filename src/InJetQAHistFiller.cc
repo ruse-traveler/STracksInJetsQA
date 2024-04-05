e6e3ea8889443685d41b5640aab69f10b37b5785
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-04-05 15:57:10 -0400

    Start filling in-jet methods

--- src/InJetQAHistFiller.cc
+++ src/InJetQAHistFiller.cc
@@ -10,4 +10,30 @@
 
 #define TRACKSINJETSQAMAKER_INJETQAHISTFILLER_CC
 
+    // loop over jet constituents
+    auto csts = jet -> get_comp_vec();
+    for (
+      auto cst = csts.begin();
+      cst != csts.end();
+      ++cst
+    ) {
+
+      // skip if cst is not a track
+      uint32_t src = cst -> first;
+      if (src != Jet::SRC::TRACK) continue;
+
+      // grab track
+      SvtxTrack* track = m_trkMap -> get(cst -> second);
+
+      // collect cst info
+      CstQAContent cstContent {
+        .ptCst   = track -> get_pt(),
+        .qualCst = track -> get_quality()
+      };
+
+      // fill histograms
+      FillHistograms(Type::All, cstContent);
+
+    }  // end cst loop
+
 // end ------------------------------------------------------------------------
